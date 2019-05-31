ad43e5bbf89174d50dfe1e779eed03d75ea3b738
VitaliIvanov <Neir0o@mail.ru>
2019-05-31 12:57:24 +0300

    changes to toopakkumised

--- sites/all/themes/impact_theme/templates/views-view-fields--t-pakkumised.tpl.php
+++ sites/all/themes/impact_theme/templates/views-view-fields--t-pakkumised.tpl.php
@@ -24,13 +24,26 @@
  * @ingroup views_templates
  */
 ?>
-<?php foreach ($fields as $id => $field): ?>
-  <?php if (!empty($field->separator)): ?>
+<?php foreach ($fields as $id => $field) : ?>
+  <?php if (!empty($field->separator)) : ?>
     <?php print $field->separator; ?>
+    
   <?php endif; ?>
 
   <?php print $field->wrapper_prefix; ?>
     <?php print $field->label_html; ?>
     <?php print $field->content; ?>
   <?php print $field->wrapper_suffix; ?>
+  <?php if (strpos($field->content, 'date-display-single') !== false) : ?>
+  <?php foreach ($field->handler->view->result as $zxc => $asd) : ?>
+  <?php if ($asd->nid == $field->raw) : ?>
+  
+  <?php $endTime = $asd->field_field_application_submission[0]['raw']['value'];
+  if ($endTime < time()) : ?>
+<div class ="aegnudtxt">AEGNUD</div>
+  <?php endif ?>
+    <?php endif; ?>
+    <?php endforeach; ?>
+    <?php endif; ?>
+    
 <?php endforeach; ?>
